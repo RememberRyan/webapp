82ef17cd6c287ec94c9768cfa151ae5faa79029c
Ryan <hiRyan@protonmail.com>
2018-11-06 19:19:50 +0200

    Add user servlet: stage 1 Shows us the JSP - the front end, which contains the form Then the user fills in the form With the form we can then use the 'submit' button which utilises ‘doPost’

---
+++ src/main/java/servlets/AddUserServlet.java
@@ -0,0 +1,24 @@
+package servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Ryan Alexander on 06/11/2018.
+ */
+public class AddUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("views/newUser.jsp");
+        requestDispatcher.forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/views/newUser.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Alex
+  Date: 06/11/2018
+  Time: 18:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>User</title>
+</head>
+<body>
+<h1>Enter information about the new user</h1>
+<form method="post">
+    <label>Name</label>
+    <input type="text" name="userName" title="Name">
+
+    <label>Password</label>
+    <input type="password" name="password" title="Password">
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
